36141d0c92a98762eb98f4ed1cdf18d9a37fee5a
Max Boguslavsky <521205+maxbogus@users.noreply.github.com>
2022-02-08 22:09:05 +0300

    Update Main.kt
    implemented random field generation.

--- Main.kt
+++ Main.kt
@@ -1,13 +1,34 @@
 package minesweeper
 
+import kotlin.random.Random
+
 fun main() {
-    println(""".X.......
-.....X..X
-....X....
-......X..
-..X......
-....X....
-..X......
-..X......
-......X..""")
+    println("How many mines do you want on the field?")
+    val limit = readLine()!!.toInt()
+    val field: MutableList<MutableList<Int>> = mutableListOf()
+    repeat(9) {
+        field.add(mutableListOf(0, 0, 0, 0, 0, 0, 0, 0, 0))
+    }
+    var counter = 0
+
+    do {
+        val column = Random.nextInt(0, 9)
+        val row = Random.nextInt(0, 9)
+        if (field[column][row] != 1) {
+            field[column][row] = 1
+            counter++
+        }
+    } while (counter < limit)
+
+    drawField(field)
+}
+
+private fun drawField(field: MutableList<MutableList<Int>>) {
+    for (line in field) {
+        var output: String = ""
+        for (number in line) {
+            output += if (number == 0) "." else "X"
+        }
+        println(output)
+    }
 }

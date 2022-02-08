f5794cbfe44674b5a0e2e8dc5fa6210816bbc86b
Max Boguslavsky <521205+maxbogus@users.noreply.github.com>
2022-02-09 02:51:39 +0300

    Update Main.kt
    reworked field generation

--- Main.kt
+++ Main.kt
@@ -2,33 +2,43 @@ package minesweeper
 
 import kotlin.random.Random
 
+private const val FIELD_SIZE = 9
+
 fun main() {
     println("How many mines do you want on the field?")
     val limit = readLine()!!.toInt()
-    val field: MutableList<MutableList<Int>> = mutableListOf()
-    repeat(9) {
-        field.add(mutableListOf(0, 0, 0, 0, 0, 0, 0, 0, 0))
-    }
-    var counter = 0
+    val field: MutableList<MutableList<String>> = generateField()
 
-    do {
-        val column = Random.nextInt(0, 9)
-        val row = Random.nextInt(0, 9)
-        if (field[column][row] != 1) {
-            field[column][row] = 1
-            counter++
-        }
-    } while (counter < limit)
-
+    fillFieldWithMines(field, limit)
     drawField(field)
 }
 
-private fun drawField(field: MutableList<MutableList<Int>>) {
-    for (line in field) {
-        var output: String = ""
-        for (number in line) {
-            output += if (number == 0) "." else "X"
+private fun generateField(): MutableList<MutableList<String>> {
+    val field: MutableList<MutableList<String>> = mutableListOf()
+    repeat(FIELD_SIZE) {
+        field.add(mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."))
+    }
+    return field
+}
+
+private fun fillFieldWithMines(
+    field: MutableList<MutableList<String>>,
+    limit: Int
+) {
+    var counter = 0
+    do {
+        val column = Random.nextInt(0, FIELD_SIZE)
+        val row = Random.nextInt(0, FIELD_SIZE)
+        if (field[column][row] != "X") {
+            field[column][row] = "X"
+            // populate with numbers
+            counter++
         }
-        println(output)
+    } while (counter < limit)
+}
+
+private fun drawField(field: MutableList<MutableList<String>>) {
+    for (line in field) {
+        println(line.joinToString(""))
     }
 }

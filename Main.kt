47c3272bbae3548180219e4b430af8da38b010a4
Max Boguslavsky <521205+maxbogus@users.noreply.github.com>
2022-02-09 14:00:07 +0300

    Update Main.kt
    Implemented game logic

--- Main.kt
+++ Main.kt
@@ -2,7 +2,10 @@ package minesweeper
 
 import kotlin.random.Random
 
+private const val EMPTY_FIELD_SYMBOL = "."
 private const val FIELD_SIZE = 9
+private const val MINE_SYMBOL = "X"
+private const val MARKER_SYMBOL = "*"
 
 fun main() {
     println("How many mines do you want on the field?")
@@ -10,14 +13,59 @@ fun main() {
     val field: MutableList<MutableList<String>> = generateField()
 
     fillFieldWithMines(field, limit)
-    drawField(field)
+    val fieldWithHiddenMines = createFieldWithHiddenMines(field)
+
+    do {
+        drawField(fieldWithHiddenMines)
+        println("Set/delete mines marks (x and y coordinates)")
+        makeMove(fieldWithHiddenMines)
+    } while (!checkWin(fieldWithHiddenMines, field))
+
+    println("Congratulations! You found all the mines!")
+}
+
+fun checkWin(fieldWithHiddenMines: MutableList<MutableList<String>>, field: MutableList<MutableList<String>>): Boolean {
+    var result = true
+    for ((index, line) in field.withIndex()) {
+        var actualResult = ""
+        for (ch in fieldWithHiddenMines[index]) {
+            actualResult += if (ch == MARKER_SYMBOL) MINE_SYMBOL else ch
+        }
+        val expectedResult = line.joinToString("")
+        if (actualResult != expectedResult) {
+            result = false
+            break
+        }
+    }
+    return result
+}
+
+private fun makeMove(fieldWithHiddenMines: MutableList<MutableList<String>>) {
+    val input = readLine()!!.split(" ").map { it.toInt() }.toList()
+    val symbol = fieldWithHiddenMines[input[0] - 1][input[1] - 1]
+    if (symbol in "1".."9") {
+        println("There is a number here!")
+    } else {
+        fieldWithHiddenMines[input[0] - 1][input[1] - 1] =
+            if (symbol == EMPTY_FIELD_SYMBOL) MARKER_SYMBOL else EMPTY_FIELD_SYMBOL
+    }
+}
+
+private fun createFieldWithHiddenMines(field: MutableList<MutableList<String>>): MutableList<MutableList<String>> {
+    val copiedMinefield = mutableListOf<MutableList<String>>()
+    for (line in field) {
+        val modifiedLine = mutableListOf<String>()
+        line.forEach { modifiedLine.add(if (it == MINE_SYMBOL) EMPTY_FIELD_SYMBOL else it) }
+        copiedMinefield.add(modifiedLine)
+    }
+    return copiedMinefield
 }
 
 private fun generateField(): MutableList<MutableList<String>> {
     val field: MutableList<MutableList<String>> = mutableListOf()
     repeat(FIELD_SIZE) {
         val line = mutableListOf<String>()
-        repeat(FIELD_SIZE) { line.add(".") }
+        repeat(FIELD_SIZE) { line.add(EMPTY_FIELD_SYMBOL) }
         field.add(line)
     }
     return field
@@ -31,8 +79,8 @@ private fun fillFieldWithMines(
     do {
         val column = Random.nextInt(0, FIELD_SIZE)
         val row = Random.nextInt(0, FIELD_SIZE)
-        if (field[column][row] != "X") {
-            field[column][row] = "X"
+        if (field[column][row] != MINE_SYMBOL) {
+            field[column][row] = MINE_SYMBOL
             // populate with numbers
             val lowRowLimit = if (row >= 1) row - 1 else 0
             val highRowLimit = if (row <= 7) row + 1 else 8
@@ -40,9 +88,9 @@ private fun fillFieldWithMines(
             val rightColumnLimit = if (column <= 7) column + 1 else 8
             for (rowAroundMine in lowRowLimit..highRowLimit) {
                 for (columnAroundMine in leftColumnLimit..rightColumnLimit) {
-                    if (field[columnAroundMine][rowAroundMine] == ".") {
+                    if (field[columnAroundMine][rowAroundMine] == EMPTY_FIELD_SYMBOL) {
                         field[columnAroundMine][rowAroundMine] = "1"
-                    } else if (field[columnAroundMine][rowAroundMine] != "X") {
+                    } else if (field[columnAroundMine][rowAroundMine] != MINE_SYMBOL) {
                         field[columnAroundMine][rowAroundMine] = "${field[columnAroundMine][rowAroundMine].toInt() + 1}"
                     }
                 }
@@ -54,7 +102,12 @@ private fun fillFieldWithMines(
 }
 
 private fun drawField(field: MutableList<MutableList<String>>) {
+    println(" │123456789│")
+    println("—│—————————│")
+    var counter = 1
     for (line in field) {
-        println(line.joinToString(""))
+        println("$counter|${line.joinToString("")}|")
+        counter++
     }
+    println("—│—————————│")
 }
